1e76b7d18a95301594c0432545de3d105dfb79cf
Matias Espince <m.espince@gmail.com>
2024-09-16 14:59:08 -0300

    clase0

---
+++ main.js
@@ -0,0 +1,62 @@
+//1.1number
+764
+745.456
+//Datos Numeros: Enteros o Decimamles.
+
+//1.2 String:
+
+//"esto es un string"
+//'Esto es un String'
+
+//1.3 Boolean
+true
+false
+//pa ahcer bucles y condicionales
+
+//1.4 Undifined
+undefined
+//Valor asignado a variable cuando no se le da otro valor
+
+//1.5 Null
+null
+//Ausencia intencional de un Valor
+
+//VARIABLES
+//Algo modificable
+let alumno;
+console.log(typeof alumno);
+//Asignar valor
+alumno = "Matias"
+
+let profesor = "Coder"
+
+//CONSTANTE
+//No se modifican
+const nacimiento = 1995;
+console.log(nacimiento);
+
+//2 Tipo de Objeto
+
+//2.1Array: coleccion de datos
+let array = [1, "hola", true, null, undefined, [1, 2, 3]]
+
+console.log(array);
+
+console.log(array.length);
+//2.2 Objetos:
+
+let perro = {
+    nombre: "Firulais",
+    edad: 5,
+    vacunado: true,
+    raza: "ladrador",
+}
+console.log(perro)
+console.log(perro.nombre);
+
+perro.nombre= "Rex";
+
+
+
+
+
